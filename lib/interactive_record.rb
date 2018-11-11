3e89129b9b94f11f5c8eefce67f2f43e34dbf171
louis baptiste <louisvis@ymail.com>
2018-11-11 14:40:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -9,7 +9,22 @@ class InteractiveRecord
       self.to_s.downcase.pluralize
   end
 
-
+  def self.column_names
+      DB[:conn].results_as_hash = true
+      
+      sql = "PRAGMA table_info('#{table_name}')"
+      
+      table_info = DB[:conn].execute(sql)
+      column_names = []
+   
+      table_info.each do |column|
+          column_names << column["name"]
+    end
+    
+      column_names.compact
+      
+  end
+  
   def values_for_insert
       values = []
       self.class.column_names.each do |col_name|
