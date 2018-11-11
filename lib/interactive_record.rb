dcb749a0cb6d3146c2dfefb51b719cc81134496a
louis baptiste <louisvis@ymail.com>
2018-11-11 14:35:58 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,16 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+
+
+
+
+
+
+  def self.find_by_name(name)
+      sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'" 
+      DB[:conn].execute(sql)
+  end
+
 end
