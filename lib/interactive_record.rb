626eb25bd5546dc1c205255fe223409b708e75e0
louis baptiste <louisvis@ymail.com>
2018-11-11 14:51:52 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,6 +8,10 @@ class InteractiveRecord
   def self.table_name
       self.to_s.downcase.pluralize
   end
+  
+  def table_name_for_insert
+      self.class.table_name
+  end
 
   def initialize(options={})
       options.each do |property, value|
@@ -41,7 +45,7 @@ class InteractiveRecord
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
-  
+
   def values_for_insert
       values = []
       self.class.column_names.each do |col_name|
