fd2ca061337a6e909c473c024395d0ea5c82d618
louis baptiste <louisvis@ymail.com>
2018-11-11 14:43:44 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,7 +8,7 @@ class InteractiveRecord
   def self.table_name
       self.to_s.downcase.pluralize
   end
-  
+
   def initialize(options={})
       options.each do |property, value|
           self.send("#{property}=", value)
